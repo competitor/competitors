09133eaf6d9eedb3bdf7f548ea635cebd1703bc9
maot <maot@andrew.cmu.edu>
2015-05-25 00:06:38 +0800

    index logic updated

--- competitors/views.py
+++ competitors/views.py
@@ -32,19 +32,19 @@ from random import randint
 
 def home(request):
 	context = {}
-	count1 = Team.objects.all().count()-5
-	count2 = Player.objects.all().count()-5
-	print request.user
+	#print request.user
 	if request.user.id > 0: # If user has logged in
 		userprofile = UserProfile.objects.get(user=request.user)
-		teams = Team.objects.filter(followers__user=userprofile,followers__is_active=True)
-		players = Player.objects.filter(followers__user=userprofile,followers__is_active=True)
+		teams = Team.objects.filter(followers__user=userprofile,followers__is_active=True).order_by("-followers__credits")[0:6]
+		players = Player.objects.filter(followers__user=userprofile,followers__is_active=True).order_by("-followers__credits")[0:6]
 	else: # Anonymous user
+		count1 = Team.objects.all().count()-5
+		count2 = Player.objects.all().count()-5
 		start1=randint(0,max(count1,1))
 		end1 = start1+5;
 		start2 = randint(0,max(count2,1))
 		end2 = start2+5
-		teams = Team.objects.annotate(num=(Count('followers'))).order_by('-num')[start1:end1]
+		teams = Team.objects.annotate(num=(Count('followers'))).order_by('-num')[0:5]
 		players = Player.objects.annotate(num=(Count('followers'))).order_by('-num')[start2:end2]
 	context['teams'] = teams
 	context['players'] = players
