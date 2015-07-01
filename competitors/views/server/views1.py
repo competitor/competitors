735368e6c64cfb9b424279bc5dc8d55fa5c277c8
maot <maot@andrew.cmu.edu>
2015-07-01 11:28:23 -0400

    delete some in  views1.py

--- competitors/views/server/views1.py
+++ competitors/views/server/views1.py
@@ -116,161 +116,161 @@ def loginself(request):
 # 	return HttpResponse(data, content_type="text/plain")
 
 
-@transaction.atomic
-def register(request):
-    context = {}
-    context['signuppage'] = True
-    if request.method == 'GET':
+# @transaction.atomic
+# def register(request):
+#     context = {}
+#     context['signuppage'] = True
+#     if request.method == 'GET':
 
-        context['form'] = RegistrationForm()
-        return render(request, 'registration/register.html', context)
+#         context['form'] = RegistrationForm()
+#         return render(request, 'registration/register.html', context)
 
-    # Creates a bound form from the request POST parameters and makes the 
-    # form available in the request context dictionary.
-    form = RegistrationForm(request.POST)
-    context['form'] = form
+#     # Creates a bound form from the request POST parameters and makes the 
+#     # form available in the request context dictionary.
+#     form = RegistrationForm(request.POST)
+#     context['form'] = form
 
-    # Validates the form.
-    if not form.is_valid():
-        return render(request, 'registration/register.html', context)
+#     # Validates the form.
+#     if not form.is_valid():
+#         return render(request, 'registration/register.html', context)
     
 
   
-    new_user = User.objects.create_user(username = form.cleaned_data['username'], 
-                                        password = form.cleaned_data['password1'],
-                                        first_name = form.cleaned_data['first_name'],
-                                        email = form.cleaned_data['email'],
-                                        last_name=form.cleaned_data['last_name']
-                                      )
+#     new_user = User.objects.create_user(username = form.cleaned_data['username'], 
+#                                         password = form.cleaned_data['password1'],
+#                                         first_name = form.cleaned_data['first_name'],
+#                                         email = form.cleaned_data['email'],
+#                                         last_name=form.cleaned_data['last_name']
+#                                       )
     
-    # # new_user.UserInfo.age=form.cleaned_data['age']
+#     # # new_user.UserInfo.age=form.cleaned_data['age']
 
-    new_user.is_active = False
+#     new_user.is_active = False
 
-    new_user.save()
+#     new_user.save()
 
-    user = UserProfile(user = new_user)
+#     user = UserProfile(user = new_user)
 
-    user.save()   
+#     user.save()   
 
-    new_user = authenticate(username = request.POST['username'], \
-                          password = request.POST['password1'])
+#     new_user = authenticate(username = request.POST['username'], \
+#                           password = request.POST['password1'])
 
 
-    token = default_token_generator.make_token(new_user)
+#     token = default_token_generator.make_token(new_user)
 
-    email_body = """
-Welcome to COMPETITORS.  Please click the link below to
-verify your email address and complete the registration of your account:
+#     email_body = """
+# Welcome to COMPETITORS.  Please click the link below to
+# verify your email address and complete the registration of your account:
 
-  http://%s%s
-""" % (request.get_host(), 
-       reverse('confirm', args=(new_user.username, token)))
+#   http://%s%s
+# """ % (request.get_host(), 
+#        reverse('confirm', args=(new_user.username, token)))
 
-    send_mail(subject="Verify your email address",
-              message= email_body,
-              from_email="maot@andrew.cmu.edu",
-              recipient_list=[new_user.email])
+#     send_mail(subject="Verify your email address",
+#               message= email_body,
+#               from_email="maot@andrew.cmu.edu",
+#               recipient_list=[new_user.email])
 
-    context['email'] = form.cleaned_data['email']
+#     context['email'] = form.cleaned_data['email']
 
-    print new_user.is_active
+#     print new_user.is_active
  
-    return render(request, 'registration/needs-confirmation.html', context)
+#     return render(request, 'registration/needs-confirmation.html', context)
 
-@transaction.atomic
-def confirm_registration(request, username, token):
-    user = get_object_or_404(User, username=username)
+# @transaction.atomic
+# def confirm_registration(request, username, token):
+#     user = get_object_or_404(User, username=username)
 
-    # Send 404 error if token is invalid
-    if not default_token_generator.check_token(user, token):
-        raise Http404
+#     # Send 404 error if token is invalid
+#     if not default_token_generator.check_token(user, token):
+#         raise Http404
 
-    # Otherwise token was valid, activate the user.
-    user.is_active = True
-    user.save()
-    return render(request, 'registration/confirmed.html', {})
+#     # Otherwise token was valid, activate the user.
+#     user.is_active = True
+#     user.save()
+#     return render(request, 'registration/confirmed.html', {})
 
-@transaction.atomic
-def change_password(request):
-	context = {}
-	email = []
-	if request.method == 'GET':
-		return render(request, 'registration/change_password.html', context)
+# @transaction.atomic
+# def change_password(request):
+# 	context = {}
+# 	email = []
+# 	if request.method == 'GET':
+# 		return render(request, 'registration/change_password.html', context)
 
-	if 'email' in request.POST and request.POST['email']:
-		email = request.POST['email']
-	else:
-		context['errors'] = "Please input your email address"
-		return render(request, 'registration/change_password.html', context)
+# 	if 'email' in request.POST and request.POST['email']:
+# 		email = request.POST['email']
+# 	else:
+# 		context['errors'] = "Please input your email address"
+# 		return render(request, 'registration/change_password.html', context)
 
-	try:
-		new_user = User.objects.get(email=email)
-	except ObjectDoesNotExist:
-		context['errors'] = "This email has not been registered."
-		return render(request, 'registration/change_password.html', context)
+# 	try:
+# 		new_user = User.objects.get(email=email)
+# 	except ObjectDoesNotExist:
+# 		context['errors'] = "This email has not been registered."
+# 		return render(request, 'registration/change_password.html', context)
 
-	token = default_token_generator.make_token(new_user)
+# 	token = default_token_generator.make_token(new_user)
 
-	email_body = """Your username is:"""+ new_user.username+""".
-	If you would like to reset your password, please click the link below to change your password:
-	http://%s%s
-""" % (request.get_host(), 
-	reverse('confirm_change', args=(new_user.username,token)))
-	print new_user.email
-	send_mail(subject="Password Change",
-			message= email_body,
-			from_email="maot@andrew.cmu.edu",
-			recipient_list=[new_user.email])
+# 	email_body = """Your username is:"""+ new_user.username+""".
+# 	If you would like to reset your password, please click the link below to change your password:
+# 	http://%s%s
+# """ % (request.get_host(), 
+# 	reverse('confirm_change', args=(new_user.username,token)))
+# 	print new_user.email
+# 	send_mail(subject="Password Change",
+# 			message= email_body,
+# 			from_email="maot@andrew.cmu.edu",
+# 			recipient_list=[new_user.email])
 
-	context['email'] = new_user.email
+# 	context['email'] = new_user.email
 
 
-	return render(request, 'registration/password_change_request_sent.html', context)
+# 	return render(request, 'registration/password_change_request_sent.html', context)
 
-@transaction.atomic
-def confirm_change(request, username, token):
-    user = get_object_or_404(User, username=username)
+# @transaction.atomic
+# def confirm_change(request, username, token):
+#     user = get_object_or_404(User, username=username)
 
-    # Send 404 error if token is invalid
-    if not default_token_generator.check_token(user, token):
-        raise Http404
+#     # Send 404 error if token is invalid
+#     if not default_token_generator.check_token(user, token):
+#         raise Http404
 
-    form = ChangePasswordForm(initial={'username': username})
-    context = {}
-    context['form'] = form
-    context['username'] = user.username
-    # Otherwise token was valid, activate the user.
-    # user.is_active = True
-    # user.save()
-    return render(request, 'registration/change_password_form.html', context)
+#     form = ChangePasswordForm(initial={'username': username})
+#     context = {}
+#     context['form'] = form
+#     context['username'] = user.username
+#     # Otherwise token was valid, activate the user.
+#     # user.is_active = True
+#     # user.save()
+#     return render(request, 'registration/change_password_form.html', context)
 
-@transaction.atomic
-def change_password_done(request):
-	context = {}
-	username = {}
-	if "username" in request.POST and request.POST['username'] :
-		username = request.POST['username']
-	else:
-		return render(request, 'competitors/index.html')
-	user = User.objects.get(username = username)
+# @transaction.atomic
+# def change_password_done(request):
+# 	context = {}
+# 	username = {}
+# 	if "username" in request.POST and request.POST['username'] :
+# 		username = request.POST['username']
+# 	else:
+# 		return render(request, 'competitors/index.html')
+# 	user = User.objects.get(username = username)
 
-# Send 404 error if token is invalid
-	# if not default_token_generator.check_token(user, token):
-	# 	raise Http404
-	form = ChangePasswordForm(request.POST)
-	if not form.is_valid():
-		context['errors'] = "Please input two same passwords"
-		form = ChangePasswordForm(initial={'username': username})
-		context['form'] = form
-		return render(request, 'registration/change_password_form.html', context)
+# # Send 404 error if token is invalid
+# 	# if not default_token_generator.check_token(user, token):
+# 	# 	raise Http404
+# 	form = ChangePasswordForm(request.POST)
+# 	if not form.is_valid():
+# 		context['errors'] = "Please input two same passwords"
+# 		form = ChangePasswordForm(initial={'username': username})
+# 		context['form'] = form
+# 		return render(request, 'registration/change_password_form.html', context)
 	
-	user.set_password(request.POST['password1'])
-	user.save()
+# 	user.set_password(request.POST['password1'])
+# 	user.save()
 	
 	
 
-	return redirect(home)
+# 	return redirect(home)
 
 
 
