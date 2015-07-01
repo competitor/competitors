1144307595b187b5bb53cd0cd9ed0963e8dd34bc
maot <maot@andrew.cmu.edu>
2015-07-01 13:35:11 -0400

    add team/player page to server.py

--- competitors/views/server/server.py
+++ competitors/views/server/server.py
@@ -23,3 +23,97 @@ def home(request):
 	context['players'] = players
 	context['homepage'] = True
 	return render(request,'competitors/index.html',context)
+
+def team_page(request,id):
+	context = {}
+	print request.GET
+	if 'cid' in request.GET and request.GET['cid']:
+		cid = request.GET['cid']
+		context['cid']=cid
+		print cid
+	if 'page' in request.GET and request.GET['page']:
+		page = request.GET['page']
+	else:
+		page = 1;
+	context['page'] = page
+	team = []
+	errors = []
+	matches = []
+	try:
+		team = Team.objects.get(id=id)
+		matcheshome = Match.objects.filter(home = team)
+		matchesaway = Match.objects.filter(away = team)
+		matches = matcheshome | matchesaway
+	except ObjectDoesNotExist:
+		errors.append('This team does not exist')
+	form = PostForm()
+	news = News.objects.filter(team__id=id)
+	start = (int(page)-1)*5
+	end = int(page)*5
+	posts = Post.objects.filter(team__id=id).order_by("-time")[start:end]
+	totalposts = Post.objects.filter(team__id=id).count()
+	context['totalposts'] = totalposts
+	try:
+		follow = Follow.objects.get(user__user__username=request.user.username,team__id=id)
+		context['follow'] = follow
+	except ObjectDoesNotExist:
+		context['not_follow'] = "not_follow"
+	if request.user.has_perm('can_manage',team):
+		context['can_manage'] = True
+		print True
+	else:
+		context['can_manage'] = False
+		print False
+	players = Player.objects.filter(team__id=id)
+	context['players'] = players
+	context['team'] = team
+	context['form'] = form
+	context['matches'] = matches
+	context['news'] = news
+	context['posts'] = posts
+	return render(request,'competitors/team.html',context)
+
+def player_page(request,id):
+	context = {}
+	if 'cid' in request.GET and request.GET['cid']:
+		cid = request.GET['cid']
+		context['cid']=cid
+	if 'page' in request.GET and request.GET['page']:
+		page = request.GET['page']
+	else:
+		page = 1;
+	context['page'] = page
+	player = []
+	errors = []
+	posts = []
+	# matches = []
+	try:
+		player = Player.objects.get(id=id)
+	except ObjectDoesNotExist:
+		errors.append('This team does not exist')
+	try:
+		team = Team.objects.get(id=player.team.id)
+		matcheshome = Match.objects.filter(home = team)
+		matchesaway = Match.objects.filter(away = team)
+		matches = matcheshome | matchesaway
+	except ObjectDoesNotExist:
+		errors.append('This team does not exist')
+	form = PostForm()
+	news = News.objects.filter(player__id=id)
+	start = (int(page)-1)*5
+	end = int(page)*5
+	posts = Post.objects.filter(player__id=id).order_by("-time")[start:end]
+	totalposts = Post.objects.filter(player__id=id).count()
+	context['totalposts'] = totalposts
+	try:
+		follow = Follow.objects.get(user__user__username=request.user.username,player__id=player.id)
+		context['follow'] = follow
+	except ObjectDoesNotExist:
+		context['not_follow'] = "not_follow"
+	context['player'] = player
+	context['form'] = form
+	context['matches'] = matches
+	context['news'] = news
+	context['posts'] = posts
+
+	return render(request,'competitors/player.html',context)
